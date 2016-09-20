2ffd6b59f571829720f9ceee863b92e0cf71a818
Mike McGuire <MoonshotMike@gmail.com>
2016-09-20 05:43:30 -0600

    Initial commit

---
+++ POJO Practice - Dog/src/Dog.java
@@ -0,0 +1,36 @@
+public class Dog
+	{
+	private int age;
+	private String name;
+
+	public Dog(int a, String n)
+		{
+		age = a;
+		name = n;
+		}
+
+	public int getAge()
+		{
+		return age;
+		}
+
+	public void setAge(int age)
+		{
+		this.age = age;
+		}
+
+	public String getName()
+		{
+		return name;
+		}
+
+	public void setName(String name)
+		{
+		this.name = name;
+		}
+
+//	public String toString()
+//		{
+//		return name + " is " + age;
+//		}
+	}
---
+++ POJO Practice - Dog/src/DogRunner.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class DogRunner
+	{
+	static ArrayList <Dog> kennel = new ArrayList <Dog>();
+	public static void main(String[] args)
+		{
+		kennel.add(new Dog(7, "Fred"));
+		kennel.add(new Dog(3, "Sprinkles"));
+		
+		System.out.println(kennel);
+
+		System.out.println(kennel.get(0).getName());
+		System.out.println(kennel.get(1).getName());
+
+		kennel.get(0).setName("Q");
+		kennel.get(1).setAge(4);
+		
+		System.out.println(kennel);
+		}
+
+	}
